63be4e39e4258a4616097c84d67dfcac85753a77
Adwait <anivandikar95@gmail.com>
2024-06-22 11:29:35 +0530

    changes

---
+++ Python/mario-less-py/mario-less.py
@@ -0,0 +1,7 @@
+print("This program Will print a Right-Angle Pyramid");
+hight = int(input("What should be the hight of the Pyramid be: "));
+for i in hight:
+    print("#");
+    #for j in hight:
+    #    print(" ");
+
